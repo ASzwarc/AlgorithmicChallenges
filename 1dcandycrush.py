e98e62af335f97059e30f1c7c51f37e5e53c02b9
ASzwarc <szwarc.adam@gmail.com>
2019-09-30 23:58:04 +0200

    feature: fixture with test and challenge solver (not working yet).

---
+++ 1dcandycrush.py
@@ -0,0 +1,55 @@
+"""
+Solution for 1D Candy Crush challenge.
+
+Task:
+Remove adjacent elements if they have same value and there is 3 or more of them
+
+Example:
+Input -> Output
+A B C C C D -> A B D
+A B B C C C B D -> A D
+A B B C C C B B D -> A D
+"""
+from typing import List
+import unittest
+
+
+def solver(input_list: List[str]) -> List[str]:
+
+    class Element():
+        def __init__(self, elem):
+            self._elem = elem
+            self._count = 1
+
+    element_list: List[Element] = []
+
+    # Algorithm:
+    # Iterate over list and check last element in element_list. If current
+    # element is same as last element in element_list then increment element's
+    # counter. Check next elements if element is same as previous one.
+    # When elements differ, then check last elements counter and if counter is
+    # equal or bigger than 3 then delete elements.
+
+    for elem in input_list:
+        pass
+
+    return [element._elem for element in element_list]
+
+
+class Test1DCandyCrushSolver(unittest.TestCase):
+
+    def test_first_case(self):
+        self.assertListEqual(solver(["A", "B", "C", "C", "C", "D"]),
+                                    ["A", "B", "D"])
+
+    def test_second_case(self):
+        self.assertListEqual(solver(["A", "B", "B", "C", "C", "C", "B", "D"]),
+                                    ["A", "D"])
+
+    def test_third_case(self):
+        self.assertListEqual(solver(
+            ["A", "B", "B", "C", "C", "C", "B", "B", "D"]), ["A", "D"])
+
+
+if __name__ == "__main__":
+    unittest.main()
